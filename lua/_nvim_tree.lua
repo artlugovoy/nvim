e501ba92057763dff631424fcfdb4e574fdd7211
Arthur Lugovoy <artlugovoy@gmail.com>
2023-09-08 01:15:50 +0300

    Configure treesitter

--- lua/_nvim_tree.lua
+++ lua/_nvim_tree.lua
@@ -7,13 +7,16 @@ vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+
   view = {
     side = 'right',
     width = 40
   },
+
   renderer = {
     group_empty = true,
   },
+
   filters = {
     dotfiles = true,
   },
