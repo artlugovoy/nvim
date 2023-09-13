bd300dfdccce64fdc1fee52c81cf933fd68471e2
Arthur Lugovoy <artlugovoy@gmail.com>
2023-09-14 02:38:50 +0300

    Add fugitive mappings

---
+++ lua/_fugitive.lua
@@ -0,0 +1,4 @@
+-- Mappings
+vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+vim.keymap.set("n", "<leader>gm", ":Git mergetool<CR>")
+vim.keymap.set("n", "<leader>gl", ":silent! Git log<CR>:bot copen<CR>")
