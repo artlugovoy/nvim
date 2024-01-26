139f81e6b19beb3fc74855b90f9931e152e7cddd
Arthur Lugovoy <artlugovoy@gmail.com>
2024-01-26 02:50:12 +0200

    bump LSP Zero to v3.x

--- lua/_lsp.lua
+++ lua/_lsp.lua
@@ -1,5 +1,13 @@
 local lsp = require('lsp-zero').preset({})
 
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    lsp.default_setup,
+    jdtls = lsp.noop
+  }
+})
+
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
--- lua/_plugins.lua
+++ lua/_plugins.lua
@@ -55,7 +55,7 @@ return require('packer').startup(function(use)
 
   use {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    branch = 'v3.x',
     requires = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},
