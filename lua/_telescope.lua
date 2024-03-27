f21f6dd3b44fd2521f2ae76f666230f57f15fe58
Arthur Lugovoy <artlugovoy@gmail.com>
2024-03-28 01:40:40 +0200

    chore: update telescope to 0.1.6

--- lua/_telescope.lua
+++ lua/_telescope.lua
@@ -7,38 +7,20 @@ require('telescope').setup{
         ["<C-j>"] = require('telescope.actions').move_selection_next,
         ["<C-k>"] = require('telescope.actions').move_selection_previous
       }
-    }
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-        -- even more opts
+    },
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      prompt_position = "top",
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        vertical = { width = 0.2 },
+        mirror = false,
       }
-
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
-    }
-  },
-  pickers = {
-    find_files = {
-      theme = "dropdown"
-    },
-    live_grep = {
-      theme = "dropdown"
-    },
-    buffers = {
-      theme = "dropdown"
     }
   }
 }
@@ -48,7 +30,3 @@ require("telescope").load_extension("ui-select")
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
-
-vim.keymap.set('i', '<C-j>', builtin.buffers, {})
-vim.keymap.set('i', '<leader>vb', builtin.buffers, {})
-
