9d543a403f71013d88213abb562246cb2ad1ae9d
Arthur Lugovoy <artlugovoy@gmail.com>
2025-08-14 18:08:07 +0300

    Update mason configuration

--- lua/_lsp.lua
+++ lua/_lsp.lua
@@ -1,8 +1,2 @@
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup()
-    end,
-  },
-})
+require("mason").setup()
+require("mason-lspconfig").setup()
